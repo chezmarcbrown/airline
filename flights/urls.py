27fdf20fe0eaf08fc451c58511e6eec19afa4cc4
chezmarcbrown <marc@chezbrown.org>
2023-10-25 16:39:14 -0800

    checkpoint-1 booking passengers

--- flights/urls.py
+++ flights/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = "flights"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:flight_id>', views.flight, name="flight")
+    path('<int:flight_id>', views.flight, name="flight"),
+    path('book/<int:flight_id>', views.book, name='book'),
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import Http404
 
-from .models import Flight
+from .models import Flight, Passenger
 
 def index(request):
     return render(request, "flights/index.html",
@@ -21,3 +21,20 @@ def flight(request, flight_id):
     return render(request, "flights/flight.html",
                   { 'flight': flight,
                    'passengers': flight.passengers.all()})
+
+def flight(request, flight_id):
+    flight = get_object_or_404(Flight, id=flight_id)
+    np = Passenger.objects.all()
+    print(np)
+    return render(request, "flights/flight.html",
+                  { 'flight': flight,
+                   'passengers': flight.passengers.all(),
+                   'non_passengers': Passenger.objects.exclude(flights=flight).all()})
+
+def book(request, flight_id):
+    if request.method == "POST":
+        p_id = request.POST["passenger"]
+        p = Passenger.objects.get(pk=int(p_id))
+        flight = get_object_or_404(Flight, id=flight_id)
+        p.flights.add(flight)
+    return redirect('flights:flight', flight_id=flight_id)
