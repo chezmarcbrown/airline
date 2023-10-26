bbcfbb393ed8dddaf0d18be28d64b0f87c808caf
chezmarcbrown <marc@chezbrown.org>
2023-10-26 05:52:32 -0800

    fixed bug in initidb.py

--- flights/scripts/initdb.py
+++ flights/scripts/initdb.py
@@ -7,10 +7,14 @@ def run():
     with open(AIRPORTS_FILENAME) as incsvfile:
         reader = csv.DictReader(incsvfile)
         for row in reader:
-            print(f'Processing row: {row}')
+            print(f'Processing row: {row}', end="...")
             code = row['code']
             city = row['city']
-            # if not Airport.objects.exists(code=code, city=city):
-            #     a=Airport(code=code, city=city)
-            #     a.save()
-            Airport.objects.get_or_create(code=code, city=city)
+            #if not Airport.objects.exists(code=code, city=city):
+            if not Airport.objects.filter(code=code, city=city).exists():
+                a=Airport(code=code, city=city)
+                a.save()
+                print("saved.")
+            else:
+                print("ignored; already in system.")
+            # Airport.objects.get_or_create(code=code, city=city)
